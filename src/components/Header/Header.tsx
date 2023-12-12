a91b82858b1fb36cc216b77154890f092aae5dd0
ssakib4040 <ssakib4040@gmail.com>
2023-12-13 03:08:30 +0600

    Link changed for mobile device

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -100,13 +100,11 @@ export default function Header() {
               className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
             >
               <li>
-                <a>Homepage</a>
+                <a>Change Password</a>
               </li>
+
               <li>
-                <a>Portfolio</a>
-              </li>
-              <li>
-                <a>About</a>
+                <a>Logout</a>
               </li>
             </ul>
           </div>
