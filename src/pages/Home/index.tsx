d5dfda9c17d6ebafc7d54648e0749763af893b49
ssakib4040 <ssakib4040@gmail.com>
2023-12-14 02:30:43 +0600

    Projects added in homepage

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,3 +1,78 @@
+const employees = [
+  {
+    active: true,
+    name: "Md Shahinur Rahman",
+    position: "Managing Director",
+    projects: ["Bulk.ly", "Bulk.sms", "Bulk.email"],
+  },
+
+  {
+    active: true,
+    name: "Employee 1",
+    position: "Chief Financial Officer",
+    projects: ["UX-Internal"],
+  },
+
+  {
+    active: false,
+    name: "Employee 2",
+    position: "Chief Technology Officer",
+    projects: ["Mediusware.us"],
+  },
+
+  {
+    active: true,
+    name: "Employee 3",
+    position: "Chief Operating Officer",
+    projects: ["script-bot"],
+  },
+
+  {
+    active: false,
+    name: "Employee 4",
+    position: "Operations Coordinator",
+    projects: ["data-hub"],
+  },
+
+  {
+    active: false,
+    name: "Employee 5",
+    position: "Software Engineer",
+    projects: ["data-hub", "web-core"],
+  },
+
+  {
+    active: true,
+    name: "Employee 6",
+    position: "Project Manager",
+    projects: ["niservices - uptimeiq"],
+  },
+
+  {
+    active: true,
+    name: "Employee 7",
+    position: "UI/UX Designer",
+    projects: ["HR", "AI Doc Builder"],
+
+  },
+
+  {
+    active: true,
+    name: "Employee 8",
+    position: "Frontend Developer (NextJS)",
+    projects: ["BASIS-Members", "HR"],
+
+  },
+
+  {
+    active: false,
+    name: "Employee 9",
+    position: "Frontend Developer (ReactJS)",
+    projects: ["ERP"],
+
+  },
+];
+
 export default function Home() {
   return (
     <div className="grid lg:grid-cols-2 gap-3 py-3">
@@ -7,34 +82,27 @@ export default function Home() {
             {/* head */}
             <thead>
               <tr>
-                <th></th>
+                <th>#</th>
                 <th>Name</th>
-                <th>Job</th>
-                <th>Favorite Color</th>
+                <th>Position</th>
               </tr>
             </thead>
             <tbody>
               {/* row 1 */}
-              <tr>
-                <th>1</th>
-                <td>Cy Ganderton</td>
-                <td>Quality Control Specialist</td>
-                <td>Blue</td>
-              </tr>
-              {/* row 2 */}
-              <tr>
-                <th>2</th>
-                <td>Hart Hagerty</td>
-                <td>Desktop Support Technician</td>
-                <td>Purple</td>
-              </tr>
-              {/* row 3 */}
-              <tr>
-                <th>3</th>
-                <td>Brice Swyre</td>
-                <td>Tax Accountant</td>
-                <td>Red</td>
-              </tr>
+
+              {employees.map((employee, index) => (
+                <tr key={index}>
+                  <th>
+                    <div
+                      className={`h-2 w-2 ${
+                        employee.active ? "bg-green-400" : "bg-red-400"
+                      } rounded-full`}
+                    ></div>
+                  </th>
+                  <td>{employee.name}</td>
+                  <td>{employee.position}</td>
+                </tr>
+              ))}
             </tbody>
           </table>
         </div>
@@ -46,34 +114,31 @@ export default function Home() {
             {/* head */}
             <thead>
               <tr>
-                <th></th>
+                <th>#</th>
                 <th>Name</th>
-                <th>Job</th>
-                <th>Favorite Color</th>
+                <th>Projects</th>
               </tr>
             </thead>
             <tbody>
               {/* row 1 */}
-              <tr>
-                <th>1</th>
-                <td>Cy Ganderton</td>
-                <td>Quality Control Specialist</td>
-                <td>Blue</td>
-              </tr>
-              {/* row 2 */}
-              <tr>
-                <th>2</th>
-                <td>Hart Hagerty</td>
-                <td>Desktop Support Technician</td>
-                <td>Purple</td>
-              </tr>
-              {/* row 3 */}
-              <tr>
-                <th>3</th>
-                <td>Brice Swyre</td>
-                <td>Tax Accountant</td>
-                <td>Red</td>
-              </tr>
+
+              {employees.map((employee, index) => (
+                <tr key={index}>
+                  <th>
+                    <div
+                      className={`h-2 w-2 ${
+                        employee.active ? "bg-green-400" : "bg-red-400"
+                      } rounded-full`}
+                    ></div>
+                  </th>
+                  <td>{employee.name}</td>
+                  <td>
+                    {employee.projects?.map((project, index) => (
+                      <div key={index}>{project}</div>
+                    ))}
+                  </td>
+                </tr>
+              ))}
             </tbody>
           </table>
         </div>
